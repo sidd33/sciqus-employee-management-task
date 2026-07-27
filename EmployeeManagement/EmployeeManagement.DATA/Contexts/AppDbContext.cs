6c8cfe6d129abf99e21133dc27572d17ca1c7a34
ishaposs <ishavp05@gmail.com>
2026-07-27 19:14:00 +0530

    Finished Employee data layer

---
+++ EmployeeManagement/EmployeeManagement.DATA/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.DATA.Contexts
+{
+	public partial class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			ConfigureDepartmentEmployee(modelBuilder);
+		}
+	}
+}
