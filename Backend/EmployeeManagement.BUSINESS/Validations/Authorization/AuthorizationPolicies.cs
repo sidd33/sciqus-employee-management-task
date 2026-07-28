2b818a86746f54f724d86447ad0d886e9398a644
ishaposs <ishavp05@gmail.com>
2026-07-28 11:54:46 +0530

    Add authorization policy infrastructure

---
+++ Backend/EmployeeManagement.BUSINESS/Validations/Authorization/AuthorizationPolicies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.BUSINESS.Validations.Authorization
+{
+    internal class AuthorizationPolicies
+    {
+    }
+}
